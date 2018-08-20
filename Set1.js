add5808ff7ab7976ad25399adf30b331d3e44774
Shane Lupton <slupton89@gmail.com>
2018-08-20 13:39:41 -0600

    added year of birth to greeting

--- Set1.js
+++ Set1.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return (`Hi my name is ${name} and I'm ${age} years old`);
+  const yearOfBirth = 2018 - age;
+  return (`Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`);
 }
 
 const greeting1 = createGreeting(name , age);
