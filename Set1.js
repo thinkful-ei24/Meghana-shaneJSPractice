3e59ebfae95d31256fd02ddb2632e04e48cac155
Shane Lupton <slupton89@gmail.com>
2018-08-20 14:44:33 -0600

    added error checking to functions

--- Set1.js
+++ Set1.js
@@ -1,9 +1,25 @@
 'use strict';
 
-function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;
-  return (`Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`);
+function getYearOfBirth(age) {
+
+  return 2018 - age;
 }
 
-const greeting1 = createGreeting(name , age);
-console.log(greeting1);
+function createGreeting(name, age) {
+  if(name.length < 1 || typeof name !== 'string') {
+    throw new Error('Name can not be empty or not a string');
+  }
+  if (age < 0 || isNaN(age) ) {
+    throw new Error('Age can not be negative or not a number');
+  }
+  return (`Hi my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}`);
+}
+
+try {
+
+
+  const greeting1 = createGreeting('John' , 30);
+  console.log(greeting1);
+} catch(e) {
+  console.error(e.message);
+}
