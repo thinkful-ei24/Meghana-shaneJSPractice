192f2fd5908e714edea13c73a4c181c3cea01d32
Vaibhav Joshi <vaibhavjoshi@Vaibhavs-iMac-907.local>
2018-08-20 15:34:44 -0400

    Changed JS file

--- Set1.js
+++ Set1.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function createGreeting() {
-  return 'Hi, my name is Shane and I\'m 29 years old';
+function createGreeting(name, age) {
+  return (`Hi my name is ${name} and I'm ${age} years old`);
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting(name , age);
 console.log(greeting1);
