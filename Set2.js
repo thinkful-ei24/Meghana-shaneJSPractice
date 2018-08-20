c91291a56a5ac7b1959d044900de49c422895484
Shane Lupton <slupton89@gmail.com>
2018-08-20 15:53:53 -0600

    added daysInMonth function

--- Set2.js
+++ Set2.js
@@ -16,3 +16,58 @@ function toInfinity(num) {
     console.log('Staying home');
   }
 }
+
+function daysInMonth(month, leapyear) {
+
+  switch (month) {
+
+  case 'January':
+    return 'January has 31 days';
+
+  case 'February':
+    if(!leapyear) {
+      return 'February has 28 days';
+    } else {
+      return 'Fenruary has 29 days during a leap year';
+    }
+
+  case 'March':
+    return 'March has 31 days';
+
+  case 'April':
+    return 'April has 30 days';
+
+  case 'May':
+    return 'May has 31 days';
+
+  case  'June':
+    return 'June has 30 days';
+
+  case  'July':
+    return 'July has 31 days';
+
+  case 'August':
+    return 'August has 31 days';
+
+  case 'September':
+    return 'September has 30 days';
+
+  case 'October':
+    return 'October has 31 days';
+
+  case 'November':
+    return 'November has 30 days';
+
+  case 'December':
+    return 'December has 31 days';
+
+  default:
+    throw new Error('Must provide a valid month');
+  }
+}
+
+try {
+  console.log(daysInMonth('February'));
+} catch(e) {
+  console.error(e.message);
+}
