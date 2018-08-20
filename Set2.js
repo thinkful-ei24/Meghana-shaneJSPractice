3ff719a49fd7d38217d558891c02de7330037971
Vaibhav Joshi <vaibhavjoshi@Vaibhavs-iMac-907.local>
2018-08-20 17:52:17 -0400

    added the crackingTheCode function

--- Set2.js
+++ Set2.js
@@ -16,3 +16,29 @@ function toInfinity(num) {
     console.log('Staying home');
   }
 }
+function crackingTheCode(message) {
+    const word = message.split('');
+    console.log(word);
+    const arr =[];
+
+
+    if (word[0] === "a"){
+        arr.push (word[1]);}
+    if( word[0] === "b") {
+        arr.push (word[2]);
+    }
+    if (word[0]=== "c") {
+        arr.push (word[3]);
+    }
+    if (word[0]=== "d"){
+        arr.push( word[4]);
+    }
+    else arr.push (" ");
+
+
+return arr.join("");
+
+}
+crackingTheCode("craft block argon meter bells brown croon droop")
+
+
