f4b1f021eb599ba6e11df48b8b307bb0ceb685f5
Shane Lupton <slupton89@gmail.com>
2018-08-20 15:07:41 -0600

    added toInfinity function

--- Set2.js
+++ Set2.js
@@ -1,4 +1,18 @@
-function jediName(firstName,lastName) {
-    return  lastName.slice(0,3)+firstName.slice(lastName.length-2,lastName.length-1)
+'use strict';
 
+function jediName(firstName,lastName) {
+  return  lastName.slice(0,3)+firstName.slice(lastName.length-2,lastName.length-1);
+
+}
+
+function toInfinity(num) {
+  if(!isFinite(num)) {
+    console.log('And beyond');
+  } else if(isFinite(num) && num < 0) {
+    console.log('To negative infinity');
+  } else if(isFinite(num) && num > 0) {
+    console.log('To infinity');
+  } else if(num === 0) {
+    console.log('Staying home');
+  }
 }
