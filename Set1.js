b0c5c95a53dd335a0e2af355770ecaa2cba6b05a
Shane Lupton <slupton89@gmail.com>
2018-08-20 13:05:38 -0600

    greeting function returns string

--- Set1.js
+++ Set1.js
@@ -1,6 +1,8 @@
+'use strict';
 
 function createGreeting() {
-  console.log('Hi, my name is Shane and I'm 29 years old);
+  return 'Hi, my name is Shane and I\'m 29 years old';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
